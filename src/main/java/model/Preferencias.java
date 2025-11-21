432c99fd5b898df8633b779dc8dc8b805a9132fa
jclmntl <joaozinhocle@hotmail.com>
2025-11-21 00:42:30 -0300

    Implementa logica de preferencias/restricoes e testa o codigo na main

--- src/main/java/model/Preferencias.java
+++ src/main/java/model/Preferencias.java
@@ -4,26 +4,58 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Preferencias {
+    //preferencias
     private Turno turnoPreferido;
     private List<String> professoresPreferidos;
+    //restricoes
+    private List<String> professoresEvitados;
+    private List<Horario> horariosEvitados;
+    private int maxCreditos;
 
     public Preferencias() {
         this.professoresPreferidos = new ArrayList<>();
+        this.professoresEvitados = new ArrayList<>();
+        this.horariosEvitados = new ArrayList<>();
+        this.maxCreditos = 30;
     }
 
-    public void adicionarProfessorPreferido(String nome){
-        this.professoresPreferidos.add(nome);
-    }
-
-    public void setTurnoPreferido(Turno turno){
-        this.turnoPreferido = turno;
-    }
-
-    public Turno getTurnoPreferido() {
+public Turno getTurnoPreferido() {
         return turnoPreferido;
+    }
+
+    public void setTurnoPreferido(Turno turnoPreferido) {
+        this.turnoPreferido = turnoPreferido;
     }
 
     public List<String> getProfessoresPreferidos() {
         return professoresPreferidos;
+    }
+
+    public void adicionarProfessorPreferido(String nome) {
+        this.professoresPreferidos.add(nome);
+    }
+
+    public List<String> getProfessoresEvitados() {
+        return professoresEvitados;
+    }
+
+    public void adicionarProfessorEvitado(String nome) {
+        this.professoresEvitados.add(nome);
+    }
+
+    public List<Horario> getHorariosBloqueados() {
+        return horariosEvitados;
+    }
+
+    public void adicionarHorarioBloqueado(Horario horario) {
+        this.horariosEvitados.add(horario);
+    }
+
+    public int getMaxCreditos() {
+        return maxCreditos;
+    }
+
+    public void setMaxCreditos(int maxCreditos) {
+        this.maxCreditos = maxCreditos;
     }
 }
