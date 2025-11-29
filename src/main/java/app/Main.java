2e5542d34ca85052c23bf17b3308226a13964a23
Mariana-banana <marianab.ercolani@gmail.com>
2025-11-28 22:42:58 -0300

    Adiciona testes para os modulos restantes e rename da main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import app.ui.AppGUI; 
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("testeeee");
 
